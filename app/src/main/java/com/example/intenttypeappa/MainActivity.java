b45e64f189413a36ccf76e9b9b7f37f422e1681f
ahmed.zien <ahmed.zien@gmail.com>
2019-10-25 23:12:09 +0200

    finish make IntentTypeAppB

--- app/src/main/java/com/example/intenttypeappa/MainActivity.java
+++ app/src/main/java/com/example/intenttypeappa/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.intenttypeappa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.intenttypeappa.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sharePost(View view) {
+        Intent intent = new Intent(this,SharePostActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void sendEmail(View view) { //Explicit Intent
+        Intent intent =new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("message","this is a simple message");
+        startActivity(intent);
     }
 }
